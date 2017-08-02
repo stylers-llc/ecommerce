31e0420265a30b27627bf8fb1e175f9aeae701c6
Nasztanovics Dávid <dnasztanovics@gmail.com>
2017-08-02 14:53:29 +0200

    repair auth login

--- src/Views/productShow.blade.php
+++ src/Views/productShow.blade.php
@@ -8,7 +8,7 @@
         <h1>{{$product['name']['en']}}</h1>
         <p>{{$product['descriptions']['short_description']['en']}}</p>
 
-        @if (Auth::check() || $product['price'])
+        @if (Auth::check() == true && $product['price'])
             <p>
                 <h4>Price: {{$product['price']}}</h4>
                 <button class="btn btn-primary btn-sm add-to-cart" data-product-id="{{$product['id']}}"><span class="glyphicon glyphicon-shopping-cart"></span></button>

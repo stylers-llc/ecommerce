40fd3811116f63346110ce2984b6014a2b6c6247
Nasztanovics Dávid <dnasztanovics@gmail.com>
2017-08-29 17:20:24 +0200

    is active not required

--- src/Manipulators/ProductSetter.php
+++ src/Manipulators/ProductSetter.php
@@ -24,7 +24,6 @@ class ProductSetter extends Setter
     public static $rules = [
         'required' => [
             'product_type',
-            'is_active',
             'name'
         ],
         'rules' => [
